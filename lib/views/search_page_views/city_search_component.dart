11f98c24c2f3ea463a56ad5d2159bc56abbbd756
Ritvik Kosuri <kosur005@umn.edu>
2023-04-26 11:46:53 -0500

    Setting up dropdown menu

--- lib/views/search_page_views/city_search_component.dart
+++ lib/views/search_page_views/city_search_component.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class CitySearchPage extends StatefulWidget {
   const CitySearchPage({Key? key}) : super(key: key);
 
@@ -10,12 +9,39 @@ class CitySearchPage extends StatefulWidget {
 
 class _CitySearchPageState extends State<CitySearchPage> {
 
+
+  String? dropdownValue;
+  var CityPicked;
+  List<String> Cities = <String>[
+    'Akron, OK',
+    'Albany, NY',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Search for Jobs Based on City'),
-      ),
+        appBar: AppBar(
+          title: Text('Search for Jobs Based on City'),
+        ),
+        body: Center(
+          child: DropdownButtonFormField<String>(
+              hint: Text("Select A City"),
+              value: CityPicked,
+              items: Cities.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+              onChanged: (value) {
+                setState(() {
+                  CityPicked = value;
+                });
+              }),
+        )
     );
   }
+
 }
+
+
