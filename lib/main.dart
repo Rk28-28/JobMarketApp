1d2df0a6199b1b666277b00b022a326a10e2b561
Noah Kimbler <kimbl056@d.umn.edu>
2023-04-27 14:39:12 -0500

    Added UI for login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:groupb_final/login_page.dart';
 
 import 'home_page.dart';
 
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+      home: LoginPage(),
     );
   }
 }
